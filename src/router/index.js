2b6648cfc91976065a70066d8acbfc8ccf7759d5
super-fox <1396303552@qq.com>
2020-08-09 10:33:35 +0800

    登录页面完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,37 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
+// import Login from '../components/Login'
+const Login = () => import('../components/Login')
+const Home = () => import('../components/Home')
 Vue.use(VueRouter)
 
 const routes = [
+  //路由重定向
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/login'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/home',
+    component: Home
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+//挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next()
+  //获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('./login')
+  next()
 })
 
 export default router
