ee1c6e37589c6fab92556999215ef158d4d4edab
super-fox <1396303552@qq.com>
2020-08-14 21:17:56 +0800

    报表展示

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,7 @@ const Params = () => import('../components/goods/Params')
 const List = () => import('../components/goods/List')
 const Add = () => import('../components/goods/Add')
 const Order = ()=>import('../components/order/Order')
+const Report =()=> import('../components/report/Report')
 Vue.use(VueRouter)
 
 const routes = [
@@ -38,7 +39,8 @@ const routes = [
       {path: '/params', component:Params},
       {path: '/goods', component:List},
       {path: '/goods/add', component:Add},
-      {path: '/orders', component:Order}
+      {path: '/orders', component:Order},
+      {path: '/reports', component:Report}
     ]
   },
 
